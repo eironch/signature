88927913d5d47976d38e805e4659454c4d26c776
Cheiron <109048315+eironch@users.noreply.github.com>
2025-05-31 22:25:04 +0800

    fix: resolve dependency problem

--- server/config/database.js
+++ server/config/database.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import mysql2 from "mysql2"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,6 +14,7 @@ dotenv.config();
 
 const sql = new Sequelize(process.env.DB_URI, {
 	dialect: "mysql",
+	dialectModule: mysql2,
 	dialectOptions: {
 		ssl: {
 			ca: fs.readFileSync(pemPath),
